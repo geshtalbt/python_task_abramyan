5d24b41ae43f11d5201b3485a26049c54d1ff9dc
geshtalbt <geshtalbt@gmail.com>
2026-09-13 13:49:29 +0300

    Modify calculations to include exponentiation and division
    Updated calculations to use exponentiation and added division.

--- python_task10.py
+++ python_task10.py
@@ -2,9 +2,9 @@ a = float(input('enter number: '))
 b = float(input('enter number: '))
 if a==0 or b==0:
     print('Error: Please enter non-zero numbers.')
-minus = a - b
-plus = a + b
-multiply = a * b
+minus = a**a - b**b
+plus = a**a + b**b
+division = a**a / b**b
 print('minus = ', minus)
 print('plus = ', plus)
-print('multiply = ', multiply)
+print('division = ', division)
